80fc66cc68f6aae5beed83013606aeb945982118
Coda <codanevergiveup@gmail.com>
2024-11-18 16:09:00 +0800

    Ensure a consistent format for remarks.

--- LoadingManager/_ILoadingShow.cs
+++ LoadingManager/_ILoadingShow.cs
@@ -7,8 +7,8 @@ namespace UnityGameFramework
     /// The interface for loading show.
     /// </summary>
     /// <remarks>
-    /// Register this interface with the Loading module, and let the module handle the loading process with the loading show.
-    /// You don't need to worry about whether 'Hide' is called in 'Show' or 'Show' is called in 'Hide'; the Loading module will handle it.
+    /// <para>Register this interface with the Loading module, and let the module handle the loading process with the loading show.</para>
+    /// <para>You don't need to worry about whether 'Hide' is called in 'Show' or 'Show' is called in 'Hide'; the Loading module will handle it.</para>
     /// </remarks>
     public interface _ILoadingShow
     {
@@ -20,7 +20,7 @@ namespace UnityGameFramework
         /// Play the show before the loading function starts running.
         /// </summary>
         /// <remarks>
-        /// The loading function will run right after the '_complete' action is called.
+        /// <para>The loading function will run right after the '_complete' action is called.</para>
         /// </remarks>
         void Show(Action _complete);
         /// <summary>
